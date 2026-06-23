77892366afedcea6da0a26bf8d268a204b9f5cd4
Tapanhaz <tapanhaz@gmail.com>
2026-06-24 01:41:25 +0530

    printer: include api modifier in Cython type output

--- packages/pyright-internal/src/analyzer/typePrinter.ts
+++ packages/pyright-internal/src/analyzer/typePrinter.ts
@@ -910,6 +910,9 @@ function printTypeWithCythonDetails(type: Type, objName: string) {
         if (details.isPublic) {
             prefixes.push('public');
         }
+        if (details.isApi) {
+            prefixes.push('api');
+        }
         if (details.isConst) {
             prefixes.push('const');
         }
