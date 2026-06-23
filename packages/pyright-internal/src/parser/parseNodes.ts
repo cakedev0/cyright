7bc8186e0a5ced8cb5345a0ca0bbd0301ce0a830
Tapanhaz <tapanhaz@gmail.com>
2026-06-24 01:37:56 +0530

    parser: add  modifier helper for Cython nodes

--- packages/pyright-internal/src/parser/parseNodes.ts
+++ packages/pyright-internal/src/parser/parseNodes.ts
@@ -2548,6 +2548,10 @@ export namespace CTypeNode {
         return hasModifier(node, KeywordType.Public);
     }
 
+    export function isApi(node: CTypeNode) {
+        return hasModifier(node, KeywordType.Api);
+    }
+
     export function isReadOnly(node: CTypeNode) {
         return hasModifier(node, KeywordType.Readonly);
     }
